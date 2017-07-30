f657b61ee37f178a7781c810b97dac182f16a955
n303p4 <n303p4@users.noreply.github.com>
2017-07-30 19:43:22 -0400

    Minor linting fixes

--- cogs/core/ping.py
+++ cogs/core/ping.py
@@ -15,7 +15,7 @@ class Ping:
     @commands.cooldown(6, 12)
     async def ping(self, ctx):
         """Ping the bot."""
-        pingtime = int(round((datetime.datetime.utcnow() - 
+        pingtime = int(round((datetime.datetime.utcnow() -
                               ctx.message.created_at).total_seconds() * 1000, 0))
         message = f":ping_pong: {pingtime} ms!"
         await ctx.send(message)
