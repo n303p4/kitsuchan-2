3d21ed008257003fdea531bcc3320c912ce750af
n303p4 <n303p4@users.noreply.github.com>
2017-11-15 05:29:25 -0500

    Added support for tags via owoe

--- cogs/fun/ram.py
+++ cogs/fun/ram.py
@@ -12,10 +12,6 @@ from k2 import helpers
 
 systemrandom = random.SystemRandom()
 
-BASE_URL_API_TYPES = "https://api.weeb.sh/images/types"
-BASE_URL_API_TYPE = "https://api.weeb.sh/images/random?type={0}"
-BASE_URL_API_TAG = "https://api.weeb.sh/images/random?tags={0}"
-
 
 async def _generate_message(ctx, kind: str=None, user: str=None):
     """Generate a message based on the user."""
@@ -47,6 +43,7 @@ class Ram:
         self.owoe = owoe.Owoe(self.bot.config["weebsh_token"], self.bot.session)
         try:  # TODO this is hack
             self.bot.loop.run_until_complete(self.owoe.update_image_types())
+            self.bot.loop.run_until_complete(self.owoe.update_image_tags())
             self._build_commands()
         except Exception:
             pass
@@ -61,12 +58,19 @@ class Ram:
 
             helptext = f"Fetches a random image of category {key.capitalize().replace('_', ' ')}."
 
-            async def callback(self, ctx):
-                url_image = await self.owoe.random_image(type_=ctx.command.name)
-                embed = discord.Embed()
-                embed.set_image(url=url_image)
-                embed.set_footer(text="Powered by weeb.sh")
-                await ctx.send(embed=embed)
+            async def callback(self, ctx, *tags):
+                for tag in tags:
+                    if tag not in self.owoe.tags:
+                        await ctx.send("Invalid tag. Use the `weebtags` command for a list.")
+                        return
+                url_image = await self.owoe.random_image(type_=ctx.command.name, tags=tags)
+                if isinstance(url_image, str):
+                    embed = discord.Embed()
+                    embed.set_image(url=url_image)
+                    embed.set_footer(text="Powered by weeb.sh")
+                    await ctx.send(embed=embed)
+                    return
+                await ctx.send("No image matching your criteria was found.")
 
             # Ew, gross.
             command = commands.command(name=key, help=helptext)(callback)
@@ -83,16 +87,24 @@ class Ram:
         await ctx.send(embed=embed)
 
     @commands.command()
+    @commands.cooldown(6, 12, commands.BucketType.channel)
+    async def weebtags(self, ctx):
+        """List all available weeb.sh tags."""
+        embed = discord.Embed(title="List of valid weeb.sh tags")
+        embed.description = ", ".join(self.owoe.tags)[:2000]
+        await ctx.send(embed=embed)
+
+    @commands.command()
     @commands.is_owner()
     async def weebupdate(self, ctx):
-        """Update list of weeb.sh types. Bot owner only."""
+        """Update list of weeb.sh types and tags. Bot owner only."""
         status = await self.owoe.update_image_types()
+        await self.owoe.update_image_tags()
         if status:
-            await ctx.send((f"Failed to update weeb.sh types with HTTP status code {status}. "
-                            "It's possible that the endpoint is down."))
+            await ctx.send("Failed to reach the endpoint.")
             return
         else:
-            await ctx.send("Updated weeb.sh types successfully.")
+            await ctx.send("Updated from weeb.sh successfully.")
         self._build_commands()
         for type_ in self.owoe.types:
             if type_ in self.bot.all_commands.keys():
