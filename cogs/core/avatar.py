7cc901f1de33e27e26a751f2b5db24321680d030
n303p4 <n303p4@users.noreply.github.com>
2017-11-16 21:29:28 -0500

    Avatar now uses helpers.member_by_substring

--- cogs/core/avatar.py
+++ cogs/core/avatar.py
@@ -5,19 +5,23 @@
 import discord
 from discord.ext import commands
 
+from k2 import helpers
+
 
 class Avatar:
     """Avatar commands."""
 
     @commands.command()
     @commands.cooldown(6, 12)
-    async def avatar(self, ctx, *, user: discord.Member=None):
+    async def avatar(self, ctx, *, user: str=None):
         """Display a user's avatar.
         Defaults to displaying the avatar of the user who invoked the command.
 
         * user - A member who you can mention for avatar."""
         if not user:
             user = ctx.author
+        else:
+            user = helpers.member_by_substring(user)
         await ctx.send(user.avatar_url)
 
     @commands.command(aliases=["gicon", "servericon", "sicon"])
