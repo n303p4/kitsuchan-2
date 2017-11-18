ac64149c081de69f164cfd491c52771107bfb1fe
n303p4 <n303p4@users.noreply.github.com>
2017-11-17 20:44:37 -0500

    Incorrect usage of ctx.bot.shard_ids replaced

--- cogs/owner/sharding.py
+++ cogs/owner/sharding.py
@@ -16,10 +16,7 @@ class Sharding:
     @commands.is_owner()
     async def shardinfo(self, ctx, shard_id: int=None):
         embed = discord.Embed(title="Shard information")
-        if ctx.bot.shard_ids:
-            embed.add_field(name="Number of shards", value=len(ctx.bot.shard_ids))
-        else:
-            embed.add_field(name="Number of shards", value=1)
+        embed.add_field(name="Number of shards", value=len(ctx.bot.shard_count))
         embed.add_field(name="Shard serving this guild (first shard is 0)",
                         value=ctx.bot.shard_id)
         latency_string = "\n".join([f"Shard {t[0]}: {round(t[1], 2)}s" for t in ctx.bot.latencies])
