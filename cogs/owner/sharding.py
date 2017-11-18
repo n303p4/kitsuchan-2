9a00be16ab6851e37657ccb1cb5e20622bc17249
n303p4 <n303p4@users.noreply.github.com>
2017-11-17 20:41:27 -0500

    Added a shardinfo cog

---
+++ cogs/owner/sharding.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""Shard-related commands."""
+
+import discord
+from discord.ext import commands
+
+
+class Sharding:
+    """Commands that are only for the bot owner.
+
+    These commands will not show up in the help unless you're the owner.
+    """
+
+    @commands.command()
+    @commands.is_owner()
+    async def shardinfo(self, ctx, shard_id: int=None):
+        embed = discord.Embed(title="Shard information")
+        if ctx.bot.shard_ids:
+            embed.add_field(name="Number of shards", value=len(ctx.bot.shard_ids))
+        else:
+            embed.add_field(name="Number of shards", value=1)
+        embed.add_field(name="Shard serving this guild (first shard is 0)",
+                        value=ctx.bot.shard_id)
+        latency_string = "\n".join([f"Shard {t[0]}: {round(t[1], 2)}s" for t in ctx.bot.latencies])
+        embed.add_field(name="Latencies", value=latency_string)
+        embed.add_field(name="Average latency", value=f"{round(ctx.bot.latency, 2)}s")
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    """Set up the extension."""
+    bot.add_cog(Sharding())
