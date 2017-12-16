911f347f6b3da23471c7af05cf90fd3ab9127ab6
n303p4 <n303p4@users.noreply.github.com>
2017-12-15 23:56:00 -0500

    [cogs.core.avatar] Added the ability to accept a size argument.

--- cogs/core/avatar.py
+++ cogs/core/avatar.py
@@ -7,6 +7,8 @@ from discord.ext import commands
 
 from k2 import helpers
 
+VALID_SIZES = [16, 32, 64, 128, 256, 512, 1024]
+
 
 class Avatar:
     """Avatar commands."""
@@ -17,12 +19,27 @@ class Avatar:
         """Display a user's avatar.
         Defaults to displaying the avatar of the user who invoked the command.
 
-        * user - A member who you can mention for avatar."""
+        * user - A member who you can mention for avatar.
+        
+        You may also specify a size for the avatar after the username, e.g. avatar kit 512
+        """
+        size = 1024
         if not user:
             user = ctx.author
+        elif user.isdecimal():
+            size = int(user)
+            user = ctx.author
         else:
-            user = await helpers.member_by_substring(ctx, user)
-        await ctx.send(user.avatar_url)
+            user_minus_last_word, sep, new_size = user.rpartition(" ")
+            if new_size.isdecimal():
+                user = await helpers.member_by_substring(ctx, user_minus_last_word)
+                size = int(new_size)
+            else:
+                user = await helpers.member_by_substring(ctx, user)
+        try:
+            await ctx.send(user.avatar_url_as(size=size))
+        except discord.InvalidArgument:
+            await ctx.send(f"Invalid size. Valid sizes: {VALID_SIZES}")
 
     @commands.command(aliases=["gicon", "servericon", "sicon"])
     @commands.cooldown(6, 12)
