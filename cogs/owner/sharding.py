bf05c9f3528f48efe69cffd55b3dc59edb551fed
n303p4 <n303p4@users.noreply.github.com>
2017-12-17 12:00:18 -0500

    [cogs.owner.sharding.py] Fixed current shard display.

--- cogs/owner/sharding.py
+++ cogs/owner/sharding.py
@@ -18,7 +18,7 @@ class Sharding:
         embed = discord.Embed(title="Shard information")
         embed.add_field(name="Number of shards", value=ctx.bot.shard_count)
         embed.add_field(name="Shard serving this guild (first shard is 0)",
-                        value=ctx.bot.shard_id)
+                        value=ctx.guild.shard_id)
         latency_string = "\n".join([f"Shard {t[0]}: {round(t[1], 2)}s" for t in ctx.bot.latencies])
         embed.add_field(name="Latencies", value=latency_string)
         embed.add_field(name="Average latency", value=f"{round(ctx.bot.latency, 2)}s")
