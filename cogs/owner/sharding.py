0ea14ddd9263dffa46f63e85ca709a9de11c83e7
n303p4 <n303p4@users.noreply.github.com>
2017-12-17 12:04:04 -0500

    [cogs/owner/sharding.py] shardinfo command uses ms for latencies now.

--- cogs/owner/sharding.py
+++ cogs/owner/sharding.py
@@ -19,9 +19,10 @@ class Sharding:
         embed.add_field(name="Number of shards", value=ctx.bot.shard_count)
         embed.add_field(name="Shard serving this guild (first shard is 0)",
                         value=ctx.guild.shard_id)
-        latency_string = "\n".join([f"Shard {t[0]}: {round(t[1], 2)}s" for t in ctx.bot.latencies])
+        latency_string = "\n".join([f"Shard {t[0]}: {round(t[1]*1000, 2)}ms"
+                                    for t in ctx.bot.latencies])
         embed.add_field(name="Latencies", value=latency_string)
-        embed.add_field(name="Average latency", value=f"{round(ctx.bot.latency, 2)}s")
+        embed.add_field(name="Average latency", value=f"{round(ctx.bot.latency*1000, 2)}ms")
         await ctx.send(embed=embed)
 
 
