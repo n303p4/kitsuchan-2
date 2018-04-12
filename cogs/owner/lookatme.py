5bd913ac574f0ca507c8acc04ad766ac5fa22de7
n303p4 <n303p4@users.noreply.github.com>
2018-04-11 22:33:00 -0400

    Fix broken lookatme

--- cogs/owner/lookatme.py
+++ cogs/owner/lookatme.py
@@ -5,33 +5,32 @@
 BASE_URL_DBOTS_API = "https://bots.discord.pw/api/bots/{0}/stats"
 
 
-async def lookatme(ctx):
-    """Update the bot's server count on bots.discord.pw."""
-    token = ctx.bot.config.get("dbots_token", None)
-
-    if not token:
-        await ctx.send("No token specified in the config.")
-        return
-
-    url = BASE_URL_DBOTS_API.format(ctx.bot.user.id)
-
-    data = {"server_count": len(ctx.bot.guilds), "shard_id": ctx.bot.shard_id,
-            "shard_count": ctx.bot.shard_count}
-    headers = {"Authorization": token}
-
-    async with ctx.bot.session.request("POST", url, json=data, headers=headers):
-        pass
-
-
 def setup(bot):
     """Set up the extension."""
 
+    token = bot.config.get("dbots_token", None)
+
+    if not token:
+        return
+
+    async def lookatme():
+        """Update the bot's server count on bots.discord.pw."""
+
+        url = BASE_URL_DBOTS_API.format(bot.user.id)
+
+        data = {"server_count": len(bot.guilds), "shard_id": bot.shard_id,
+                "shard_count": bot.shard_count}
+        headers = {"Authorization": token}
+
+        async with bot.session.request("POST", url, json=data, headers=headers):
+            pass
+
     @bot.listen("on_guild_join")
-    async def handle_guild_join(ctx):
+    async def handle_guild_join(guild):
         """Trigger lookatme on guild join."""
-        await lookatme(ctx)
+        await lookatme()
 
     @bot.listen("on_guild_remove")
-    async def handle_guild_remove(ctx):
+    async def handle_guild_remove(guild):
         """Trigger lookatme on guild leave."""
-        await lookatme(ctx)
+        await lookatme()
